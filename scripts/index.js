16909560add2c7c1f55c75e2e45ff83b1a1e015f
Evdokim Mellin <evdockimpyatkov.ru@yandex.ru>
2022-07-18 07:29:33 +0300

    fix: исправить ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 let edit = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let form = document.querySelector('.popup__form');
 let closeButton = document.querySelector('.popup__close-button');
 let saveButton = document.querySelector('.popup__save-button');
 let name = document.querySelector('.profile__name');
@@ -7,29 +8,25 @@ let description = document.querySelector('.profile__description');
 let inputName = document.querySelector('.popup__input_type_name');
 let inputDescription = document.querySelector('.popup__input_type_description');
 
-inputName.value = name.textContent;
-inputDescription.value = description.textContent;
 
 function openPopup() {
+  inputName.value = name.textContent;
+  inputDescription.value = description.textContent;
   popup.classList.add('popup_opened');
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
-  inputName.value = name.textContent;
-  inputDescription.value = description.textContent;
 }
 
-function save() {
-  /*name.innerHTML = `${inputName.value}<img alt="Редактировать" src="./images/edit-button.svg" class="profile__edit-button">`;*/
-  /*При изменении текста с помощью textContent из элемента почему-то пропадал код кнопки
-  внесения изменений. Я не нащел другого способа сохранить его*/
-  name.textContent = `${inputName.value}`;
+function save(evt) {
+  evt.preventDefault();
+  name.textContent = inputName.value;
   description.textContent = inputDescription.value;
   closePopup();
 }
 
 edit.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', save);
+form.addEventListener('submit', save);
 
